badf40d4bd8321fb3dbacac570c6dce443c77c68
KUAN-HSUN-LI <b06209027@ntu.edu.tw>
2019-03-24 20:26:31 +0800

    a lot of bug

---
+++ Practice03/own/src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { Navigation } from './App';
+import './vendor/bootstrap/css/bootstrap.css';
+import './vendor/font-awesome/css/font-awesome.min.css';
+import './css/agency.css';
+import * as serviceWorker from './serviceWorker';
+
+const index = ['Services', 'Portfolio', 'About', 'Team', 'Contact'];
+
+const services = { head: ['E-Commerce', 'Responsive Design', 'Web Security'], main: ['...', '...', '...'] };
+
+// ReactDOM.render(<Navagation index={index} />);
+ReactDOM.render(<App index={index} services={services} />, document.getElementById('root'));
+// ReactDOM.hydrate(<Navigation index1={index1} />, document.getElementById('root'));
+// ReactDOM.render(<Navagation index1={index1} />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
